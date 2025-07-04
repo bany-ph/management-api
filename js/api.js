623500b1354b6526cb9275d4a25accd82aaea74a
bany-ph <brisbanystudy@gmail.com>
2025-07-04 12:20:12 -0500

    feat: update user management functionality and improve API integration

--- js/api.js
+++ js/api.js
@@ -1,42 +1,51 @@
 import axios from "axios"; // import axios
 
-const MAIN_URL = "http://localhost:3001/users"
+const MAIN_URL = "http://localhost:3001/users";
 
+const generateID = () => {
+  return (Date.now() + Math.random()).toString();
+};
 
-const generateID = () =>{
-    return Date.now() + Math.random();
-}
+const getData = () => {
+  return axios.get(MAIN_URL, {
+    headers: {
+      "Cache-Control": "no-cache",
+      Pragma: "no-cache",
+      Expires: "0",
+    }, // ensures that the browser does not cache the response
+    params: { t: Date.now() },
+  });
+};
 
+const getDataUser = (id) => {
+  return axios
+    .get(`${MAIN_URL}/${id}`)
 
-const getData = () => { return axios.get(MAIN_URL)}
+    .catch((err) => console.log(err));
+};
 
+const addData = (newData) => {
+  axios
+    .post(MAIN_URL, {
+      id: generateID(),
+      name: newData.name,
+      email: newData.email,
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
 
-const getDataUser = (id) => { return axios.get(`${MAIN_URL}?id=${id}`).catch((err) => console.log(err)); }
+const updateData = (id, newUpdateData) => {
+  axios
+    .patch(`${MAIN_URL}/${id}`, newUpdateData)
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
 
-const addData = (newData) => {axios.post(MAIN_URL, {
-    id: generateID,
-    name: newData.name,
-    email: newData.email
-}).catch(err => {
-    console.log(err)
-})}
+const deleteData = (id) => {
+  axios.delete(`${MAIN_URL}/${id}`).catch((err) => console.log(err));
+};
 
-const updateData = (id, newData) => {axios.patch(`${MAIN_URL}?id=${id}`, {
-
-
-
-}).catch((err) => console.log(err));}
-
-const deleteData = (id) => {axios.delete(`${MAIN_URL}/${id}`).catch((err) => console.log(err));}
-
-
-
-export{
-    MAIN_URL,
-    getData,
-    getDataUser,
-    addData,
-    updateData,
-    deleteData
-}
-
+export { MAIN_URL, getData, getDataUser, addData, updateData, deleteData };
--- js/index.js
+++ js/index.js
@@ -1,109 +1,158 @@
-import * as api from "./api.js"
-import {getData, updateData,deleteData} from "./api.js";
+import * as api from "./api.js";
+import { getData, updateData, deleteData } from "./api.js";
 
-
-
-
-async function getUsers(){ // search data in an api file
-        try{
-            const response = await getData();
-            return response.data;
-        }catch(err){
-            if(err.response){
-                console.error(err.response); //the server response with an error code
-            }else if(err.request){
-                console.error(err.request); // request something but did not response
-            }else{
-                console.error(err); // something went wrong
-            }
-        }
-}
-
-async function addUser(name, email){
-    try {
-        const newData = {
-            name: name,
-            email: email
-        };
-        await api.addData(newData);
-    }catch(err){
-
-        console.error(err)
+async function getUsers() {
+  // search data in an api file
+  try {
+    const response = await getData();
+    return response.data;
+  } catch (err) {
+    if (err.response) {
+      console.error(err.response); //the server response with an error code
+    } else if (err.request) {
+      console.error(err.request); // request something but did not response
+    } else {
+      console.error(err); // something went wrong
     }
+  }
 }
 
-async function deleteUser(id) {
+const addUser = async (name, email) => {
+  try {
+    const newData = {
+      name: name,
+      email: email,
+    };
+    return await api.addData(newData);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-    await api.deleteData(id)
-    console.log("Successfully deleted user");
-}
-async function updateUser(id){
-    await api.updateData(id);
-}
+const deleteUser = async (id) => {
+  try {
+    return await api.deleteData(id);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateUser = async (id, name, email) => {
+  try {
+    const dataToUpdate = {
+      name: name,
+      email: email
+    };
+   /* 
+    if name or email are undefined it will delete the property from the object
+   */
+
+    dataToUpdate.name === undefined && delete dataToUpdate.name;
+    dataToUpdate.email === undefined && delete dataToUpdate.email;
+
+    return await api.updateData(id, dataToUpdate);
+    
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 
 
-/*DOM*/
-const container = document.querySelector(".users");// the main container
 
-/*
-*   event submits.
-*   validate inputs
-*   create object with the input data
-*   and pass the object in paragrams
-* */
+/* DOM */
+const container = document.querySelector(".users"); // the main container
 
 const form = document.getElementById("main-form");
-form.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    const inputName = document.getElementById("name");
-    const inputEmail = document.getElementById("email");
-     
-    if((!inputName.value || !inputEmail.value)){
-        alert("fill all the inputs")
-        return;
-    }
+form.addEventListener("submit", (event) => {
+  // for add new user
+  event.preventDefault();
+  const inputName = document.getElementById("name");
+  const inputEmail = document.getElementById("email");
 
+  if (!inputName.value || !inputEmail.value) {
+    alert("fill all the inputs");
+    return;
+  }
 
-
-    addUser(inputName.value, inputEmail.value)
-        .then(res =>{
-            alert(res +  " Updated successfully ")
-    }).catch(err =>{
-            alert("Updated failed " + err);
+  addUser(inputName.value, inputEmail.value)
+    .then(() => {
+      console.log("added successfully");
+      inputName.value = "";
+      inputEmail.value = "";
+      renderUsers(); // reload page
     })
-    inputName.value = "";
-    inputEmail.value = "";
-    renderUsers();
-
-})
-
+    .catch((err) => {
+      alert("Updated failed " + err);
+    });
+});
 
 /*capture click event in icons (delete and update)*/
-container.addEventListener("click", (event) =>{
-    if(event.target.classList.contains("delete")){ // if you click in the delete button then ->
-        deleteUser(event.target.dataset.id).then(res =>{
-           alert("Successfully deleted user");
-        }).catch(err =>{
-            alert("delete failed " + err);
-        })
-    }else if(event.target.classList.contains("update")){ // if you click in the update button then ->
+container.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    // if you click in the delete button then ->
+    deleteUser(event.target.dataset.id)
+      .then(() => {
+        renderUsers();
+      })
+      .catch((err) => {
+        alert("delete failed " + err);
+      });
+  } else if (event.target.classList.contains("update")) {
+    showFormToUpdate(event);
+  }
+});
+
+const showFormToUpdate = (event) => {
+  const userId = event.target.dataset.id;
+  //only open the form that owns the user
+  const updateContainer = document.querySelector(
+    `.container-upload-form[data-id="${userId}"]`
+  );
+  updateContainer.classList.toggle("hide");
+
+  /* 
+        check the upload form
+        if the form is visible then hide it
+        if the form is hidden then show it
+        this is done by toggling the class "hide"
+        and the form is only visible for the user that owns it
+    
+    */
+  updateContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newInputName = document.querySelector(
+      `.input-name[data-id="${userId}"]`
+    );
+    const newInputEmail = document.querySelector(
+      `.input-email[data-id="${userId}"]`
+    );
+
+    let name = newInputName.value || undefined;
+    let email = newInputEmail.value || undefined;
+
+    if (!name && !email) {
+      alert("At leats one input have to be filled");
+      return;
     }
 
-    renderUsers();
-})
+    updateUser(userId, name, email)
+      .then(() => {
+        renderUsers();
+      })
+      .catch((error) => alert("Update failed " + error));
+  });
 
-
-
-
+};
 
 const renderUsers = () => {
-    getUsers().then((data) => {
-        container.innerHTML = "";
-        data.forEach((user) => {
-            const userContainer = document.createElement("div");
-            userContainer.classList.add("container-user-box");
-            userContainer.innerHTML += `
+  getUsers()
+    .then((data) => {
+      container.innerHTML = "";
+      data.forEach((user) => {
+        const userContainer = document.createElement("div");
+        userContainer.classList.add("container-user-box");
+        userContainer.innerHTML += `
              <div class="user-info">
                  <span>${user.name}</span>
                 <span>${user.email}</span>
@@ -114,21 +163,18 @@ const renderUsers = () => {
             </div>
             <div class="container-upload-form hide" data-id="${user.id}">
                     <form action="">
-                        <input  class="form-input" type="text" name="upload-name" id="upload-name" data-id="${user.id}" placeholder="Change name"></input>   
-                        <input  class="form-input" type="text" name="upload-email" id="upload-email" data-id="${user.id}" placeholder="Change Email"></input>
+                        <input  class="form-input input-name" type="text" name="upload-name" id="upload-name" data-id="${user.id}" placeholder="Change name"></input>   
+                        <input  class="form-input input-email" type="text" name="upload-email" id="upload-email" data-id="${user.id}" placeholder="Change Email"></input>
                          <button type="submit">Upload</button>
                      </form>    
             </div>
             `;
-            container.appendChild(userContainer);
-        })
-    }).catch(err =>{
-        container.innerHTML = `Error showing users : ${err}`;
+        container.appendChild(userContainer);
+      });
     })
-}
+    .catch((err) => {
+      container.innerHTML = `Error showing users : ${err}`;
+    });
+};
 
 renderUsers();
-
-
-
-
